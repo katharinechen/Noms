40d6042721b1e528e9c5889f94d75bddd6f46a6b
Katharine Chen <katharinechen.ny@gmail.com>
2016-01-10 19:19:24 -0800

    WIP server.py

--- noms/server.py
+++ noms/server.py
@@ -1,9 +1,23 @@
+"""
+Twisted Web Routing 
+"""
+from twisted.web import static
+
 from klein import Klein
+
 
 class Server(object): 
   """
   The web server
   """
   app = Klein() 
+
+  @app.route("/")
+  def index(self, request): 
+      return static.File("/Users/katharinechen/Noms/noms/templates/index.html")
+
+  @app.route("/static/", branch=True)
+  def static(self, request): 
+      return static.File("./static")
 
 resource = Server().app.resource
