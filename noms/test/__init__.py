b20c108993ab9ab00dd60ef5d3aa61d54226630b
Cory Dodt <corydodt@gmail.com>
2016-04-24 15:49:37 -0700

    nom-3 prettier reporting, auto-open browser, and test_config

---
+++ noms/test/__init__.py
@@ -0,0 +1,3 @@
+"""
+Tests for noms python code
+"""
--- noms/testit.py
+++ noms/testit.py
@@ -1,8 +1,21 @@
 """
-Test-running tool
-"""
+Test-running tool.
+------------------------------------------------------------------------        
+- If no tool options are specified at all, run all tools with all default
+options.
+                                                                                
+- Otherwise, run the specified tools only, with the specified options.
+------------------------------------------------------------------------        
+TIP: The parameters above take the extra args you want to pass into the tool
+as a string, but using one will run ONLY that tool. If you want to run several
+with the defaults and one without, try this syntax:
+                                                                                
+$ testit -p "noms.foo" -kk -cc
+""" # note - preserve whitespace in the docstring above.
+
 import shlex
 import subprocess
+import webbrowser
 
 from twisted.python.procutils import which
 
@@ -15,14 +28,14 @@ ALL_TOOLS = ['python', 'pyflakes', 'coverage']
 
 class TestIt(Main):
     """
-    Command-line options for the test-running tool
+    Run tests of various kinds.
     """
     synopsis = "testit [individual command params]"
 
     optParameters = [
-        ['python', 'p', None, 'Run python tests (use -p="" for defaults)'],
-        ['pyflakes', 'k', None, 'Run pyflakes (use -k="" for defaults)'],
-        ['coverage', 'c', None, 'Run the coverage tool (generates HTML by default) (use -c="" for defaults)'],
+        ['python', 'p', None, 'Run python tests with args (or -pp)'],
+        ['pyflakes', 'k', None, 'Run pyflakes with args (or -kk)'],
+        ['coverage', 'c', None, 'Generate pretty coverage with args (HTML by default) (or -cc)'],
     ]
 
     def __init__(self):
@@ -36,11 +49,18 @@ class TestIt(Main):
         """
         Decide what tools to run and then run them
         """
-        for tool in ALL_TOOLS:
-            if self[tool]:
-                self.tools[tool] = self[tool]
+        self.tools = {}
+        for t in ALL_TOOLS:
+            if self[t] is None:
+                continue
+            else:
+                # make -kk -pp etc. work...
+                if self.synonyms.get(self[t]) == t:
+                    self[t] = ""
 
-        if not self.tools:
+                self.tools[t] = self[t]
+
+        if len(self.tools) == 0:
             self.tools = {t: None for t in ALL_TOOLS}
 
         toolResults = {}
@@ -49,10 +69,11 @@ class TestIt(Main):
 
         for tool, extra in toolOrder:
             extra = shlex.split(extra or '')
+            print '\n', '#' * 10, tool
             rc = getattr(self, 'run_' + tool)(extra)
             toolResults[tool] = rc
 
-        print '#' * 80
+        print '\n\n', '#' * 80, '\n'
         for tool, _ in toolOrder:
             if toolResults[tool] == 0:
                 print tool.ljust(20), 'ok'
@@ -75,9 +96,22 @@ class TestIt(Main):
         Run Python coverage
         """
         coverage = which('coverage')[0]
-        _args = [coverage, 'html']
+        _args = [coverage, 'html', '--fail-under=100']
         args = _args + (extra or [])
-        return subprocess.call(args)
+        rc = subprocess.call(args)
+
+        if rc == 2:
+            print "** Coverage is incomplete"
+            webbrowser.open("htmlcov/index.html")
+
+        elif rc == 0:
+            print "Coverage is 100%"
+
+        # Other rcs are errors unrelated to coverage completion
+        else:
+            print "** Coverage runtime error"
+
+        return rc
 
     def run_pyflakes(self, extra):
         """
@@ -86,4 +120,8 @@ class TestIt(Main):
         pyflakes = which('pyflakes')[0]
         _args = [pyflakes, 'noms']
         args = _args + (extra or [])
-        return subprocess.call(args)
+        rc = subprocess.call(args)
+        if rc == 0:
+            print 'pyflakes passes'
+        return rc
+
