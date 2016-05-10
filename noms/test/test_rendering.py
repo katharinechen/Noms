98404f50a3acacea1d7c63da7ec34b931d197b48
Cory Dodt <corydodt@gmail.com>
2016-05-10 00:20:04 -0700

    noms-3 rendering coverage

--- noms/test/test_rendering.py
+++ noms/test/test_rendering.py
@@ -1,15 +1,18 @@
 """
 Test the facilities that support jinja rendering
 """
+import json
 from inspect import cleandoc
 
 from jinja2 import Template
 
 from twisted.trial import unittest
 
+from mongoengine import StringField, IntField
+
 from mock import patch
 
-from noms.test import mockConfig
+from noms.test import mockConfig, mockDatabase
 from noms import rendering, secret, recipe, urlify
 
 
@@ -62,6 +65,15 @@ class RenderableQuerySetTest(unittest.TestCase):
     """
     Cover the RenderableQuerySet
     """
+    def test_empty(self):
+        """
+        Do I correctly produce an error for empty queries?
+        """
+        with mockConfig():
+            qs = recipe.Recipe.objects()
+            rqs = rendering.RenderableQuerySet(qs)
+            self.assertRaises(rendering.EmptyQuery, rqs.render, None)
+
     def test_render(self):
         """
         Do I produce a json array from a query?
@@ -76,3 +88,27 @@ class RenderableQuerySetTest(unittest.TestCase):
             qs = recipe.Recipe.objects()
             expected = '[{"recipeYield": null, "tags": [], "name": "delicious sandwich", "author": "cory", "instructions": [], "ingredients": [], "urlKey": "delicious-sandwich-cory-", "user": "katharinechen.ny@gmail.com"}, {"recipeYield": null, "tags": [], "name": "delicious soup", "author": "cory", "instructions": [], "ingredients": [], "urlKey": "delicious-soup-cory-", "user": "katharinechen.ny@gmail.com"}]'
             self.assertEqual(rendering.RenderableQuerySet(qs).render(None), expected)
+
+
+class RenderableDocumentTest(unittest.TestCase):
+    def test_safe(self):
+        with mockDatabase():
+            class Doc(rendering.RenderableDocument):
+                pass
+
+            self.assertRaises(NotImplementedError, Doc().safe)
+
+    def test_render(self):
+        with mockDatabase():
+            class Doc(rendering.RenderableDocument):
+                safeValue = StringField()
+                badValue = StringField()
+                intValue = IntField()
+
+                def safe(self):
+                    return {'safe': self.safeValue, 'int': self.intValue}
+
+            doc = Doc(safeValue='good', badValue='no', intValue=12)
+            self.assertEqual(doc.render(None),
+                    json.dumps({'safe': 'good', 'int': 12})
+                    )
