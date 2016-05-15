e5cf04da3471b2fdf67b1c77d8efe5ef360f7829
Katharine Chen <katharinechen.ny@gmail.com>
2016-05-14 22:44:16 -0700

    noms-2 Saves into the database

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,6 +1,20 @@
+// DOMCOntentLoaded = this event is fired when the initial HTML document has been completely loaded and parsed 
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-    alert("hello");
-  }, false);
+    var url = ""; 
+    var nomsbook = "http://localhost:8080/api/bookmarklet?uri=";
+
+    var checkPageButton = document.getElementById('checkPage');
+
+    // save the url for the current page 
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            url = tabs[0].url;
+        });    
+
+    // send bookmarklet the link of the recipe link
+    openTab = function() { 
+        chrome.tabs.create({url: nomsbook +  encodeURIComponent(url)});
+    }; 
+    checkPageButton.addEventListener('click', openTab, false);
 }, false);
+
+// https://developer.chrome.com/extensions/overview
