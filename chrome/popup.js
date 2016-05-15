de4c094497314fef1be75a41eb7adf794f9bbf1b
Katharine Chen <katharinechen.ny@gmail.com>
2016-05-14 17:32:31 -0700

    noms-2 Backbone for chrome extension

---
+++ chrome/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+    alert("hello");
+  }, false);
+}, false);
