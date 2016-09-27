3f7b78dfc004ad912bb3945f8272da1a028f6207
Cory Dodt <corydodt@gmail.com>
2016-09-27 11:02:12 -0700

    noms-2 fix jshint

--- chrome/scripts/noms/clipper.js
+++ chrome/scripts/noms/clipper.js
@@ -1,3 +1,4 @@
+/* globals chrome */
 'use strict';
 
 // root angular app
@@ -5,34 +6,34 @@ var app = angular.module("clipper", []);
 
 var Clipper = app.controller("Clipper", ['$scope', '$http', function ($scope, $http) {
     var nomsbook = "http://localhost:8080/api/bookmarklet?uri=";
-    $scope.saved = false; 
-    $scope.showButton = true; 
-    $scope.domainURL = "http://localhost:8080"
+    $scope.saved = false;
+    $scope.showButton = true;
+    $scope.domainURL = "http://localhost:8080";
 
-    // user clicked the save button 
-    $scope.saveme = function() { 
+    // user clicked the save button
+    $scope.saveme = function() {
         chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-            $scope.url = tabs[0].url; 
-            $http({method: 'GET', url: nomsbook + $scope.url}).then(function successCallback(response) { 
+            $scope.url = tabs[0].url;
+            $http({method: 'GET', url: nomsbook + $scope.url}).then(function successCallback(response) {
                 $scope.message = "";
 
-                var data = response['data'];  
-                if (data['status'] === "error") {
-                    $scope.message = 'statusError'; 
-                    $scope.responseError = data['message']; 
-                    $scope.showButton = false; 
-                } 
-
-                if (data['recipes'].length > 0) {
-                    $scope.saved = true; 
-                    $scope.showButton = false; 
-                    $scope.recipes = data['recipes']; 
+                var data = response.data;
+                if (data.status === "error") {
+                    $scope.message = 'statusError';
+                    $scope.responseError = data.message;
+                    $scope.showButton = false;
                 }
-            }, function errorCallback(response) { 
-                // traceback occur on the server 404, 500 
-                $scope.message = 'error'; 
-                $scope.showButton = false; 
-            }); 
-        }); 
-    }; 
+
+                if (data.recipes.length > 0) {
+                    $scope.saved = true;
+                    $scope.showButton = false;
+                    $scope.recipes = data.recipes;
+                }
+            }, function errorCallback(response) {
+                // traceback occur on the server 404, 500
+                $scope.message = 'error';
+                $scope.showButton = false;
+            });
+        });
+    };
 }]);
--- static/js/controllers/recipenew.js
+++ static/js/controllers/recipenew.js
@@ -1,21 +1,21 @@
-'use strict'; 
+'use strict';
 
 // controls the list of recipes
 app.controller('NewRecipeCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.recipe = {};
 
-    $scope.update = function(recipe) { 
+    $scope.update = function(recipe) {
       // temporary
-      var recipe = Object.assign({}, recipe);     
-      recipe.ingredients = [recipe.ingredients]; 
-      recipe.instructions = [recipe.instructions]; 
+      recipe = Object.assign({}, recipe);
+      recipe.ingredients = [recipe.ingredients];
+      recipe.instructions = [recipe.instructions];
 
       $http.post('/api/recipe/create', recipe).success(function(data) {
-        $scope.ok = true;  
-        $scope.message = "Done"; 
+        $scope.ok = true;
+        $scope.message = "Done";
       }).error(function (err) {
-        $scope.ok = true; 
-        $scope.message = "ERRRORROROROR"; 
-      });  
-    }; 
-}]); 
+        $scope.ok = true;
+        $scope.message = "ERRRORROROROR";
+      });
+    };
+}]);
