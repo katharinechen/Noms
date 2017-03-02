f17e634993f2eadfdcb69822ab675c5c25b26413
Cory Dodt <corydodt@gmail.com>
2017-03-02 11:26:31 -0800

    noms-21 making noms-nginx-front configurable at build time

--- deployment/nomsbook/certbot-nomsbook.sh
+++ deployment/nomsbook/certbot-nomsbook.sh
@@ -2,6 +2,9 @@
 
 set -e 
 
+. $noms/env
+
+
 first () {
     # schedule certbot to run, and exec off to nginx
 
@@ -11,18 +14,26 @@ first () {
     exec nginx -g "daemon off;"
 }
 
+
 second () {
     # get a new cert with certbot, install the ssl config file, install the
     # renewal cron job
 
-    certbot renew || certbot certonly --webroot -w /usr/share/nginx/html \
+    certbot $NOMS_CERTBOT_FLAGS certonly --webroot -w /usr/share/nginx/html \
         -n --agree-tos -m corydodt@gmail.com \
         --preferred-challenges http-01 \
-        -d nomsbook.com
+        -d $NOMS_HOSTNAME | grep Congratulations
+
+echo > /hostname.conf << EOF
+    server_name $NOMS_HOSTNAME;
+    ssl_certificate /etc/letsencrypt/live/$NOMS_HOSTNAME/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$NOMS_HOSTNAME/privkey.pem;
+EOF
 
     mv /etc/nginx/conf.d/nomsbook.conf.ssl /etc/nginx/conf.d/nomsbook.conf
     kill -HUP 1; nginx -T
-    echo '1,13 * * * *  certbot renew' > /etc/cron.d/01certbot
+    echo '1,13 * * * *  . $noms/env && certbot $NOMS_CERTBOT_FLAGS renew' > /etc/cron.d/01certbot
 }
+
 
 "$@"
