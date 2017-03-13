289449ff0c26f3b5937478679fcd418e6833f6e2
Katharine Chen <katharinechen.ny@gmail.com>
2017-03-12 20:06:39 -0700

    Noms-33 Add angular-xeditable

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,13 @@
 'use strict';
 
 // root angular app
-var app = angular.module("noms", []);
+var app = angular.module("noms", ["xeditable"]);
+// var app = angular.module("noms", ["xeditable"]);
+
+app.run(function(editableOptions) {
+    editableOptions.theme = 'default'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
+
 
 var Preload = app.controller("Preload", ['$rootScope', '$window', function ($rootScope, $window) {
     // the nomsPreload object on the window is used by jinja templates to pass
