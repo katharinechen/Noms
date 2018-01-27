0d2e721e64695f36ec249a247465a3beb2c58d10
Katharine Chen <katharinechen.ny@gmail.com>
2018-01-27 13:23:38 -0800

    noms-33 New button that launch modal

--- static/js/controllers/recipe.js
+++ static/js/controllers/recipe.js
@@ -75,6 +75,35 @@ app.controller('Recipe', ['$scope', '$http', '$window', '$mdDialog', '$mdToast',
         )
     };
 
+    $scope.showTabDialog = function(ev) {
+        $mdDialog.show({
+          controller: DialogController,
+          templateUrl: 'tabDialog.tmpl.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:true
+        })
+            .then(function(answer) {
+              $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+              $scope.status = 'You cancelled the dialog.';
+            });
+    };
+
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+          $mdDialog.hide();
+        };
+    
+        $scope.cancel = function() {
+          $mdDialog.cancel();
+        };
+    
+        $scope.answer = function(answer) {
+          $mdDialog.hide(answer);
+        };
+    }; 
+
     // Confirmation modal for deleting a recipe
     $scope.deleteConfirm = function(ev, recipe) {
         var confirm = $mdDialog.confirm()
