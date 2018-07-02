81edb3bef6489b7cf53f501a37e9f969eaddcc7a
Katharine Chen <katharinechen.ny@gmail.com>
2018-07-01 18:59:49 -0700

    noms-33 Minor changes

---
+++ notes.md
@@ -0,0 +1,8 @@
+Things to fix:
+
+NEW RECIPE FORM:
+- When an user is not signed in and creates a recipe for the first time, there should be a button in the error message that redirects the user back to the google sign in link.
+- When an user is signed in and creates a recipe for the first time, we should not redirect them right away. It should only be redirected after the ok button is pushed.
+
+EDIT REFCIPE
+- Add ingredients and add instructions looks really bad.
--- static/js/controllers/recipenew.js
+++ static/js/controllers/recipenew.js
@@ -4,36 +4,39 @@
 app.controller('NewRecipeCtrl', ['$scope', '$http', '$mdDialog', '$mdToast', '$window', function($scope, $http,  $mdDialog, $mdToast, $window) {
     $scope.recipe = {};
 
-    // ingredients and instructions are entered in large textboxes 
+    // ingredients and instructions are entered in large textboxes
     // items could be seperated by either commas or newlines
-    // insert all items into an array 
-    $scope.parseTextBox = function(content) { 
-        return content.split(/[\n,]+/) 
-    }; 
+    // insert all items into an array
+    $scope.parseTextBox = function(content) {
+        return content.split(/[\n,]+/)
+    };
 
     $scope.saveNewRecipe = function(recipe) {
         recipe = Object.assign({}, recipe);
 
-        recipe.ingredients = $scope.parseTextBox(recipe.ingredients); 
+        recipe.ingredients = $scope.parseTextBox(recipe.ingredients);
         recipe.instructions = $scope.parseTextBox(recipe.instructions);
-        recipe.tags = $scope.parseTextBox(recipe.tags); 
+        recipe.tags = $scope.parseTextBox(recipe.tags);
 
         $http.post('/api/recipe/create', recipe).then(
             (response) => {
                 var data = response.data;
-                // when it is successfully, need to send the user to the right link 
-                // when applicable 
-                $scope.saveAlert(); 
+                // when it is successfully, need to send the user to the right link
+                // when applicable
+                $scope.saveAlert();
                 $window.location.href = '/recipes/' + data.message;
             },
-            (err) => { // errors
-                $scope.errorAlert(err.message); 
+            (err) => {
+                if (err.status === 403) {
+                    $scope.errorAlert('In order to save your recipe, you must log in to Noms first!');
+                } else {
+                    $scope.errorAlert(err.data);
+                }
             }
         );
-
     };
 
-    // Confirmation modal for saving a recipe 
+    // Confirmation modal for saving a recipe
     $scope.saveAlert = function(){
         $mdDialog.show(
             $mdDialog.alert()
@@ -45,7 +48,7 @@ app.controller('NewRecipeCtrl', ['$scope', '$http', '$mdDialog', '$mdToast', '$w
         );
     };
 
-    // Confirmation modal for saving a recipe 
+    // Confirmation modal for saving a recipe
     $scope.errorAlert = function(err){
         $mdDialog.show(
             $mdDialog.alert()
@@ -53,7 +56,7 @@ app.controller('NewRecipeCtrl', ['$scope', '$http', '$mdDialog', '$mdToast', '$w
             .clickOutsideToClose(true)
             .title('Error')
             .textContent(err)
-            .ok('You suck!')
+            .ok('Ok')
         );
-    }; 
-}]); 
+    };
+}]);
