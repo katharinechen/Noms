68021d48712d35f4e1b6f5ad18828389b13e9d07
Cory Dodt <corydodt@gmail.com>
2018-11-07 16:44:21 -0800

    noms-67 some work getting noms to launch

--- noms/secret.py
+++ noms/secret.py
@@ -4,16 +4,17 @@ Passwords, intended to be stored in the database
 from __future__ import print_function
 
 from binascii import hexlify
-import os
 import io
-
-from bson import json_util
+import os
 
 import boto3
+
+from bson import json_util
 
 from mongoengine import fields
 
 from noms import documentutil, CONFIG
+from noms.const import ENCODING
 
 
 NO_DEFAULT = object()
@@ -82,7 +83,10 @@ def loadFromS3():
 
         output = io.BytesIO()
         bucket.download_fileobj('secret_pair/secret_pair.json', output)
+        output.seek(0)
+        ff = io.TextIOWrapper(output, encoding=ENCODING)
 
         # save it to mongo
         print("Piping hot fresh secrets from bucket %r" % bucket.name)
-        SecretPair._get_collection().insert(json_util.loads(output.getvalue()))
+        doc = json_util.loads(ff.read())
+        SecretPair._get_collection().insert_one(doc)
