32723a059e40480d5d2644c7ca1c9f02731867c9
Cory Dodt <121705+corydodt@users.noreply.github.com>
2019-03-23 16:43:35 -0700

    NOMS-67 settin' up tox

---
+++ setup.py
@@ -0,0 +1,32 @@
+from inspect import cleandoc
+
+from setuptools import setup
+
+
+setup(
+    name = 'Noms',
+    packages = ['noms',],
+    version = '0.0.0',
+    description = 'Noms recipe amazement',
+    author = 'Cory Dodt',
+    author_email = 'corydodt@gmail.com',
+    url = 'https://github.com/corydodt/Noms',
+    keywords = [],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+    scripts = 'bin/do-travis-build bin/whisk bin/noms'.split(),
+    install_requires=cleandoc('''
+        boto3>=1.9.120
+        codado>=0.6.1
+        future>=0.17.1
+        gitpython>=2.1.11
+        itsdangerous>=1.1.0
+        klein>=17.10.0
+        microdata>=0.6.1
+        mongoengine>=0.17.0
+        mnemonicode>=1.4.4
+        treq>=18.6.0
+        twisted>=17.1.0
+        ''').split()
+)
