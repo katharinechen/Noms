87defa31315706e9ed047baa8e8f6284780c3c90
Cory Dodt <121705+corydodt@users.noreply.github.com>
2019-05-04 16:26:31 -0700

    NOMS-67 unpack most of postOptions into separate functions to be run with callWhenRunning

--- noms/cli.py
+++ noms/cli.py
@@ -22,6 +22,9 @@ FIXME_LOCALAPI_URL = 'http://localhost:8080'
 STATIC_FILE_PATTERNS = '*.js;*.css;*.html;*.json;*.gif;*.png;*.eot;*.woff;*.otf;*.svg;*.ttf'
 
 
+cwr = reactor.callWhenRunning
+
+
 class Run(tap.Options):
     """
     Run noms
@@ -38,35 +41,18 @@ class Run(tap.Options):
         assert alias in DBAlias
         connect(**DBHost[alias])
 
-        # compute the current static digest hash
-        staticPath = '%s/static' % os.getcwd()
-        CONFIG.staticHash = digest(staticPath)
+        # schedule these to run:
 
-        # get secrets from aws and store them in mongo
-        reactor.callWhenRunning(secret.loadFromS3)
+        # - get secrets from aws and store them in mongo
+        cwr(secret.loadFromS3)
+        # - initialize 'user' and 'localapi' users
+        cwr(userLoadLocalapi)
+        cwr(userLoadDefaultUser)
 
-        # store an internally-shared secret
-        if not secret.get('localapi', None):
-            password = secret.randomPassword()
-            secret.put('localapi', 'localapi', password)
-            print("Stored new localapi password")
-
-        # ensure that at least the special users exist
-        user.USER()
-
-        # watch for changes to static files (cache busting)
-        watchCL = "watchmedo shell-command --patterns='{pat}' --recursive --command='{cmd}' {where}"
-        watchCL = watchCL.format(
-            pat=STATIC_FILE_PATTERNS,
-            cmd='whisk digester -U %s/api/sethash/ %s' % (FIXME_LOCALAPI_URL, staticPath),
-            where=staticPath,
-            )
-        subprocess.Popen(shlex.split(watchCL), stdout=subprocess.PIPE)
-
-        # run Sass
-        sassCL = "node-sass -w static/scss/base.scss -o static/css"
-
-        subprocess.Popen(shlex.split(sassCL), stdout=subprocess.PIPE)
+        # - start watchmedo to look for staticHash changes
+        cwr(subprocessRunWatchmedo)
+        # - start node-sass to watch for scss changes and rebuild css
+        cwr(subprocessRunNodeSass)
 
         self.opt_class(MAIN_FUNC)
 
@@ -76,6 +62,52 @@ class Run(tap.Options):
 Options = Run
 
 makeService = tap.makeService
+
+
+def userLoadLocalapi():
+    """
+    At reactor startup, install the localapi user with a random password
+    """
+    # store an internally-shared secret
+    if not secret.get('localapi', None):
+        password = secret.randomPassword()
+        secret.put('localapi', 'localapi', password)
+        print("Stored new localapi password")
+
+
+def userLoadDefaultUser():
+    """
+    At reactor startup, install the default 'user' user
+    """
+    user.USER()
+
+
+def subprocessRunWatchmedo():
+    """
+    At reactor startup, run watchmedo, the process that updates the cache busting hash
+    """
+    # compute the current static digest hash
+    staticPath = '%s/static' % os.getcwd()
+    CONFIG.staticHash = digest(staticPath)
+
+    # watch for changes to static files (cache busting)
+    watchCL = "watchmedo shell-command --patterns='{pat}' --recursive --command='{cmd}' {where}"
+    watchCL = watchCL.format(
+        pat=STATIC_FILE_PATTERNS,
+        cmd='whisk digester -U %s/api/sethash/ %s' % (FIXME_LOCALAPI_URL, staticPath),
+        where=staticPath,
+        )
+    subprocess.Popen(shlex.split(watchCL), stdout=subprocess.PIPE)
+
+
+def subprocessRunNodeSass():
+    """
+    At reactor startup, run node-sass and watch scss for changes to keep the CSS files uptodate
+    """
+    # run Sass
+    sassCL = "node-sass -w static/scss/base.scss -o static/css"
+
+    subprocess.Popen(shlex.split(sassCL), stdout=subprocess.PIPE)
 
 
 def main():
